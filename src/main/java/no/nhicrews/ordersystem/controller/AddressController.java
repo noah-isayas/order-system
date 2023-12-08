3c2ddf8246f7307885c0b215037c6e2f3cea3b29
noah-isayas <noahhaile1@gmail.com>
2023-12-08 13:55:29 +0100

    Implemented address controller, post request returns 201, but with no bound customer

--- src/main/java/no/nhicrews/ordersystem/controller/AddressController.java
+++ src/main/java/no/nhicrews/ordersystem/controller/AddressController.java
@@ -1,4 +1,45 @@
 package no.nhicrews.ordersystem.controller;
 
+
+import no.nhicrews.ordersystem.model.Address;
+import no.nhicrews.ordersystem.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/addresses")
 public class AddressController {
+    private final AddressService addressService;
+
+    @Autowired
+    public AddressController(AddressService addressService){
+        this.addressService = addressService;
+    }
+    @GetMapping
+    public ResponseEntity<Page<Address>> getAllAddresses(Pageable pageable) {
+        return ResponseEntity.ok(addressService.getAllAddresses(pageable));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable Long id) {
+        return ResponseEntity.ok(addressService.getAddressById(id));
+    }
+    @PostMapping
+    public ResponseEntity<Address> addAddress(@RequestBody Address address) {
+        Address newAddress = addressService.addAddress(address);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newAddress);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddress(@PathVariable Long id, @RequestBody Address address) {
+        Address updatedAddress = addressService.updateAddress(id, address);
+        return ResponseEntity.ok(updatedAddress);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAddress(@PathVariable Long id) {
+        addressService.deleteAddress(id);
+        return ResponseEntity.noContent().build();
+    }
 }
